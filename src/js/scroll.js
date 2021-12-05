dad5d24d0606720e766f48d1a504ec8b90796662
Lionel Gareca <lionelgareca@gmail.com>
2021-12-04 21:08:57 -0300

    Añadiendo Animaciones Js

---
+++ src/js/scroll.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.contenido-header', {
+        duration: 3000,
+        origin: 'bottom', 
+        distance: '-100px'
+    });
+
+    sr.reveal('.scroll-productos', {
+        duration: 3000,
+        origin: 'left',
+        distance: '400px'
+    });
